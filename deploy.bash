47891b691920a412091c225138ab9f1cc58a9c85
Andy Wahrenberger <andywahr@microsoft.com>
2019-04-12 07:39:56 -0500

    point to correct subscription for log file

--- deploy.bash
+++ deploy.bash
@@ -7,4 +7,4 @@ terraform init
 terraform plan 
 terraform apply -auto-approve
 terraform output > ./terraform_output.txt
-az storage blob upload --file ./terraform_output.txt --container-name $1 --name terraform_output.txt --account-name saarkhitekton
+az storage blob upload --file ./terraform_output.txt --container-name $1 --name terraform_output.txt --account-name saarkhitekton --subscription bc73a756-864c-4429-8918-fe8f8eeee4a7
